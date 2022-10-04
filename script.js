04d9a8c6d64b5fc607b777cbfc2724b8d361e0f8
swaan03 <109649547+swaan-kim@users.noreply.github.com>
2022-10-04 13:27:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,15 @@ function test() {
   const loginId = document.getElementById('userid');
   const loginPw = document.getElementById('userpass');
 
-      if(loginPw.value.length<8 || loginId.value.indexOf("@")!==-1 || loginId.value.indexOf(".")!==-1){
-      alert("이메일의 @ .기호 ,비밀번호 8자 이상 작성하십시오");
+      if( loginId.value.indexOf("@")==-1 || loginId.value.indexOf(".")==-1){
+      alert("이메일에 필수 기호 '@' '.' 를 넣어 작성하십시오 ");
     }
 
+
+      else if(loginPw.value.length<8){
+      alert("비밀번호를 8자 이상 작성하십시오");
+    }
+      else{
+        alert("로그인 성공")
+      }
 }
